bce8a4e993ac4903bc0178baf1c98ce7d636ce35
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-06-26 22:20:08 -0500

    Initial commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
+import { getAuth } from 'firebase/auth';
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCSFY7bCZ3rnkBx-w0VGaFLanpvR72Tn4g",
   authDomain: "virtual-wallet-95a8f.firebaseapp.com",
@@ -13,5 +10,6 @@ const firebaseConfig = {
   appId: "1:607656454198:web:d4b5c21e58ed026abf5097"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
