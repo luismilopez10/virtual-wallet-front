4eba1dd280c80f6bb69dd3db9c22f54f3940e857
luismilopez10 <luis.miguel.lopez.10@gmail.com>
2022-06-29 18:33:56 -0500

    Merge con luis-branch

--- src/features/collaboratorSlice.ts
+++ src/features/collaboratorSlice.ts
@@ -16,7 +16,7 @@ type collaboratorType = {
     name: string | null,
     balance: number,
     contactsList:contactType[],
-    logged: boolean | undefined
+    logged: boolean
 }
 
 interface collaboratorStateType {
