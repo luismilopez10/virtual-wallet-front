6d4aca2c744b81b46ed829bb019cf9775d1ab890
SantiagoGM19 <santigm1905@gmail.com>
2022-06-30 22:31:22 -0500

    estilo basico realizado en pago de colaboradores

--- src/components/CollaboratorPayment/CollaboratorPayment.tsx
+++ src/components/CollaboratorPayment/CollaboratorPayment.tsx
@@ -18,6 +18,7 @@ function EmployeePayment() {
     const _export = useRef<ExcelExport | null>(null)
     const inputFile = useRef(null) as any
     const [isDownload, setIsDownload] = useState(false)
+    const [isUploaded, setIsUploaded] = useState(false)
     const [fileSelected, setFileSelected] = useState(false)
     const { user } = useSelector((state: RootState) => state.logged);
     const dispatch = useAppDispatch()
@@ -36,6 +37,7 @@ function EmployeePayment() {
             _export.current.save();
             setIsDownload(true)
             setCollaborators(getCollaborators)
+            setIsUploaded(false)
             console.log(getCollaborators);
 
         }
@@ -48,6 +50,7 @@ function EmployeePayment() {
             console.log(paymentContent);
             setPayments(paymentContent)
             setFileSelected(true)
+            setIsUploaded(true)
         })
     }
 
@@ -56,15 +59,18 @@ function EmployeePayment() {
             inputFile.current.value = ""
             setIsDownload(false)
             setFileSelected(false)
+            setPayments([])
+            setIsUploaded(false)
         }
     }
 
-    const payToCollaborators = () => {
+    const payToCollaborators = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
         let flag = false
         payments.forEach(payment => {
             let collaboratorFound: any = getCollaborators.find((collaborator: collaboratorType | any) => collaborator.email === payment[0])
             if (collaboratorFound) {
-                if (payment[1] >= 0) {
+                if (payment[1] >= 0 && payment[1]) {
                     let collaboratorToUpdate: collaboratorType = {
                         email: collaboratorFound.email,
                         name: collaboratorFound.name,
@@ -88,12 +94,13 @@ function EmployeePayment() {
                     dispatch(putCollaborator(collaboratorToUpdate))
                     setPayments([])
                 } else {
-                    alert(`El pago para ${payment[0]} contiene un valor no valido en el pago: ${payment[1]} , por favor cambiarlo e intentar de nuevo`)
+                    alert(`El pago para ${payment[0]} contiene un valor no valido en el pago: ${payment[1]} , por favor cambiarlo e intentar de nuevo, no habrá pago para este`)
                 }
             } else {
                 alert(`El correo ${payment[0]} no existe en la base de datos, para este usuario no hay transaccion`)
             }
         })
+        setIsUploaded(false)
     }
 
 
@@ -103,14 +110,14 @@ function EmployeePayment() {
     }
 
     return (
-        <div className='grid justify-center space-y-0'>
+        <div className='grid justify-center space-y-0 container mx-auto'>
             <div className='grid gap-y-5' style={{ marginTop: "5rem", marginBottom: "0rem" }}>
                 <h1 className='text-4xl'>Pago de colaboradores</h1>
                 <p>Por favor descarga el archivo Excel, ingresa los saldos a
                     <br />
                     pagar a cada colaborador y vuelve a montar el archivo</p>
             </div>
-            <div className='grid grid-rows-4 grid-flow-col gap-y-10 justify-center '>
+            <div className='flex flex-col space-y-16 ml-6'>
                 <div>
                     <ExcelExport data={getCollaborators} ref={_export}>
                         <ExcelExportColumn field='email' title='Correo' width={200} />
@@ -119,7 +126,7 @@ function EmployeePayment() {
                 </div>
                 <div className='grid justify-center'>
                     <button
-                        onClick={createExcel}
+                        onClick={() => { createExcel()}}
                         className='
                 border
                 border-black 
@@ -139,47 +146,53 @@ function EmployeePayment() {
                         className='w-64'
                     />
                 </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <td>Correo</td>
-                            <td>Pago</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {payments.map((payment: { toString: () => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }[]) => {
-                            return <tr>
-                                <td>{payment[0] ? payment[0].toString() : 'null'}</td>
-                                <td>{payment[1] ? payment[1].toString() : 'null'}</td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-                <div className='flex space-x-4'>
-                    <button onClick={deleteFile}
-                        disabled={!fileSelected}
-                        className='
-                        border
-                        border-black 
-                        rounded-md
-                        h-10
-                        w-40
-                        shadow-xl
-                        bg-red-500
-                        '
-                    >Quitar archivo</button>
-                    <button onClick={() => {payToCollaborators(); deleteFile()}}
-                        className='
-                        border
-                        border-black 
-                        rounded-md
-                        h-10
-                        w-40
-                        shadow-xl
-                        bg-green-500
-                        '
-                    >Pagar</button>
-                </div>
+                {isUploaded ?
+                    <div className='flex flex-col space-y-12'>
+                        <table className='table-fixed border-collapse border border-black border-separate border-spacing-2' style={{ backgroundColor: 'white' }}>
+                            <thead>
+                                <tr className=''>
+                                    <td className='border-collapse border border-black' style={{ backgroundColor: '#0e3b43', color: 'white' }}>Correo</td>
+                                    <td className='border-collapse border border-black' style={{ backgroundColor: '#0e3b43', color: 'white' }}>Pago</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {payments.map((payment: { toString: () => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }[]) => {
+                                    return <tr>
+                                        <td>{payment[0] ? payment[0].toString() : 'null'}</td>
+                                        <td style={payment[1] ? { color: 'green' } : { color: 'red' }}>{payment[1] ? "$" + payment[1].toString() : 'null'}</td>
+                                    </tr>
+                                })}
+                            </tbody>
+                        </table>
+                        <div className='flex space-x-4 justify-center'>
+                            <button onClick={deleteFile}
+                                disabled={!fileSelected}
+                                className='
+                    border
+                    border-black 
+                    rounded-md
+                    h-10
+                    w-40
+                    shadow-xl
+                    bg-red-500
+                    '
+                            >Quitar archivo</button>
+                            <button onClick={(e) => { payToCollaborators(e); deleteFile() }}
+                                className='
+                    border
+                    border-black 
+                    rounded-md
+                    h-10
+                    w-40
+                    shadow-xl
+                    bg-green-500
+                    '
+                            >Pagar</button>
+                        </div>
+                    </div>
+                    :
+                    <div></div>
+                }
             </div>
         </div>
     )
